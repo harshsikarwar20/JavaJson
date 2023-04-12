554181477b8e44d0016843bad6747aaf628e6ff4
Harsh Sikarwar <123385605+harshsikarwar20@users.noreply.github.com>
2023-04-12 22:00:00 +0530

    Create JSON.java

---
+++ JSON.java
@@ -0,0 +1,9 @@
+{
+   "categories":[],
+   "created_at":"2020-01-05 13:42:25.628594",
+   "icon_url":"https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+   "id":"rfrq6f19SaCTAAP44kBjMg",
+   "updated_at":"2020-01-05 13:42:25.628594",
+   "url":"https://api.chucknorris.io/jokes/rfrq6f19SaCTAAP44kBjMg",
+   "value":"Chuck Norris can think Amy Schumer isn't funny without being labelled a woman-hater."
+}
